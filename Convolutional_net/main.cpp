2d9a7b755391f8ad360b99dcf5fc43971c472e9a
Alexander Gomez <alviurlex@gmail.com>
2014-11-15 18:31:49 -0500

    Convolutional net v 0.0.0-1

---
+++ Convolutional_net/main.cpp
@@ -0,0 +1,17 @@
+#include "convolutional.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Convolutional w;
+    w.show();
+
+    return a.exec();
+}
+
+
+
+
+
+
